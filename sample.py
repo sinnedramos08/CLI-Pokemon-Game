93b09677e502bfd9db4f27f12fea9f5d7ddde378
sinnedramos08 <sinnedramos.org01@gmail.com>
2024-07-19 20:50:50 +0800

    deleted unnecessary files

--- sample.py
+++
@@ -1,7 +0,0 @@
-moves= [{'name': 'Bug Bite', 'damage': 60}, {'name': 'X-Scissor', 'damage': 80}, {'name': 'Signal Beam', 'damage': 75}, {'name': 'Leech Life', 'damage': 80}]
-
-
-health = 50
-health-=24
-health=max(health,0)
-print(health)
