340ec82c0439ca573aa569565fcbd54be3fceea4
sinnedramos08 <sinnedramos.org01@gmail.com>
2024-07-19 20:55:39 +0800

    added feature to control board dimension and grass counts

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from gameflow import GameFlow
 
-new_game=GameFlow(board_width=20, board_height=10, grass_count=150, steps=1, pokeballs=10, heal=10)
+board_width=input("Input Board Width: ")
+board_height=input("Input Board Height: ")
+grass_count=input("Input Grass Count: ")
+
+
+new_game=GameFlow(board_width=20, board_height=10, grass_count=150, steps=50, pokeballs=10, heal=10)
 new_game.print_intro()
 new_game.choose_new_pokemon()
 new_game.start_game()
